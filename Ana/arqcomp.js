794c4f9a5a608e25123c4d97f79cad6e5abc13bb
Ana Marques Rodrigues <ana.silva@bandtec.com.br>
2021-10-26 23:57:35 -0300

    Criação Grid Layout e JS

--- Ana/arqcomp.js
+++ Ana/arqcomp.js
@@ -1,5 +1,16 @@
-function onkeyup(){
-    var r
-    var a
-    var m
+
+    var r = r_ram.value
+    var a = a_ram.value
+    var m = m_ram.value
+
+    function word_ram(){
+    if (r == "R" && a == "A" && m == "M"){
+        r_ram.style.backgroundColor = "green";
+        a_ram.style.backgroundColor = "green";
+        m_ram.style.backgroundColor = "green";
+    } else {
+        r_ram.style.backgroundColor = "red";
+        a_ram.style.backgroundColor = "red";
+        m_ram.style.backgroundColor = "red";
+    }
 }
