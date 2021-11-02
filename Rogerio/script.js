02cd1392443f2f90cc0e48d2fcc541fcb82ec745
rogerio-alvesf <rogerio.filho@bandtec.com.br>
2021-11-01 22:37:02 -0300

    Alteração nos inputs e validação de campos

--- Rogerio/script.js
+++ Rogerio/script.js
@@ -1,12 +1,10 @@
-function verificar() {
-    var cl1n1 = c1n1.value;
-    var cl1n2 = c1n2.value;
-    var cl1n3 = c1n3.value;
-    // if (cl1n1.indexOf("c") == 1 && cl1n1.indexOf("p") == 1 && cl1n1.indexOf("u") == 1)
-    // if (cl1n1.length[1] == "c" && cl1n2.length[1] == "p" && cl1n3.length[1] == "u")
-    if (cl1n1.toLowerCase == "c" && cl1n2.toLowerCase == "p" && cl1n3.toLowerCase == "u") {
-        alert("Parabéns!");
-    } else {
-        alert("Algo está errado");
+function check_cpu() {
+    var letter_r1c1 = r1c1.value;
+    var letter_r2c1 = r2c1.value;
+    var letter_r3c1 = r3c1.value;
+    if (letter_r1c1.toLowerCase() == "c" & letter_r2c1.toLowerCase() == "p" & letter_r3c1.toLowerCase() == "u") {
+        r1c1.style.backgroundColor = "green";
+        r2c1.style.backgroundColor = "green";
+        r3c1.style.backgroundColor = "green";
     }
 }
